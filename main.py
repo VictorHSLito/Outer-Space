d1f9011ebab4fb19cb780de607ebdde4140ad7ac
Victor Hugo <victorferrarihugo@hotmail.com>
2024-02-27 20:50:36 -0300

    Game

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import game_functions as gf
 
 
 def run_game():
-    pygame.init()
+    pygame.init()           
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Invasão Alienígena")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     while True:
         gf.check_events(ship)
         ship.update()
